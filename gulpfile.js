bf4b4129f44bcce432c6a272fe727da25d461774
Vittorio Vittori <me@vittoriovittori.com>
2016-11-13 09:52:42 +0100

    Fix build version

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,7 @@
 
 /*
   D U S T M A N
-  1.10.48
+  1.10.49
 
   A Gulp 4 automation boilerplate
   by https://github.com/vitto
@@ -1441,6 +1441,6 @@ task.js = (function(){
 
 message.intro();
 config.load('>=5.4.1');
-message.verbose('Version', '1.10.48');
+message.verbose('Version', '1.10.49');
 message.verbose('Config loaded', config.file());
 tasks.init();
